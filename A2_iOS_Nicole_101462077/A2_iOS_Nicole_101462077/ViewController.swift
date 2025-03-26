0d3437285ca00007dc6d07eb8b1a700615a15dce
Nicole Milmine <122618341+n2milmin@users.noreply.github.com>
2025-03-26 18:01:46 -0400

    ViewController added searchBar, tableView, fetchRequest

--- A2_iOS_Nicole_101462077/A2_iOS_Nicole_101462077/ViewController.swift
+++ A2_iOS_Nicole_101462077/A2_iOS_Nicole_101462077/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addProductButton: UIButton!
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var products: [Product]?
+    var displayProducts: [Product]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        
+        fetchProducts()
     }
-
-
+    
+    func fetchProducts() {
+        let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
+        
+        do{
+            products = try context.fetch(fetchRequest)
+            tableView.reloadData()
+        } catch {
+            print("Error getting products: \(error)")
+        }
+    }
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return displayProducts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt index: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: index)
+        let product = displayProducts[index.row]
+        cell.textLabel?.text = product.name
+        cell.detailTextLabel?.text = product.description
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if  searchText.isEmpty {
+            displayProducts = products.prefix(1).mao { $0 }
+        } else {
+            displayProducts = products.filter( $0.name?.localizedCaseInsensitiveContains(searchText) == true || $0.description?.localizedCaseInsensitiveContains(searchText) == true)
+        }
+        tableView.reloadData()
+    }
+    
 }
 
